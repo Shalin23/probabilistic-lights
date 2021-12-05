f27b686aa1d23c52ca230164754f087ae6b84d5b
Ahmad Feroz <88190058+akhmadferoz-maqsad@users.noreply.github.com>
2021-12-05 23:00:49 +0500

    feat: Changed calculator colors

--- calculatorInterface.py
+++ calculatorInterface.py
@@ -46,7 +46,7 @@ def clear():
 gui = Tk()
 
 # set the background colour of GUI window
-gui.configure(background="#323232")
+gui.configure(background="#E6E6E6")
 
 # set the title of GUI window
 gui.title("Turing Calculator")
@@ -56,67 +56,67 @@ gui.geometry("220x270")
 
 equation = StringVar()
 
-expression_field = Entry(gui, textvariable=equation, width=11)
-expression_field.grid(columnspan=20, ipadx=50, ipady=10)
+expression_field = Entry(gui, textvariable=equation, width=11, highlightbackground='#656565', bg="#808080")
+expression_field.grid(columnspan=22, ipadx=50, ipady=10)
 
-button0 = Button(gui, text=' 0 ', fg='black', bg='red', command=lambda: press(0), height=2, width=2)
+button0 = Button(gui, text=' 0 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(0), height=2, width=2)
 button0.grid(row=5, column=1)
 
-button1 = Button(gui, text=' 1 ', fg='black', bg='red', command=lambda: press(1), height=2, width=2)
+button1 = Button(gui, text=' 1 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(1), height=2, width=2)
 button1.grid(row=4, column=0)
 
-button2 = Button(gui, text=' 2 ', fg='black', bg='red', command=lambda: press(2), height=2, width=2)
+button2 = Button(gui, text=' 2 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(2), height=2, width=2)
 button2.grid(row=4, column=1)
 
-button3 = Button(gui, text=' 3 ', fg='black', bg='red', command=lambda: press(3), height=2, width=2)
+button3 = Button(gui, text=' 3 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(3), height=2, width=2)
 button3.grid(row=4, column=2)
 
-button4 = Button(gui, text=' 4 ', fg='black', bg='red', command=lambda: press(4), height=2, width=2)
+button4 = Button(gui, text=' 4 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(4), height=2, width=2)
 button4.grid(row=3, column=0)
 
-button5 = Button(gui, text=' 5 ', fg='black', bg='red', command=lambda: press(5), height=2, width=2)
+button5 = Button(gui, text=' 5 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(5), height=2, width=2)
 button5.grid(row=3, column=1)
 
-button6 = Button(gui, text=' 6 ', fg='black', bg='red', command=lambda: press(6), height=2, width=2)
+button6 = Button(gui, text=' 6 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(6), height=2, width=2)
 button6.grid(row=3, column=2)
 
-button7 = Button(gui, text=' 7 ', fg='black', bg='red', command=lambda: press(7), height=2, width=2)
+button7 = Button(gui, text=' 7 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(7), height=2, width=2)
 button7.grid(row=2, column=0)
 
-button8 = Button(gui, text=' 8 ', fg='black', bg='red', command=lambda: press(8), height=2, width=2)
+button8 = Button(gui, text=' 8 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(8), height=2, width=2)
 button8.grid(row=2, column=1)
 
-button9 = Button(gui, text=' 9 ', fg='black', bg='red', command=lambda: press(9), height=2, width=2)
+button9 = Button(gui, text=' 9 ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(9), height=2, width=2)
 button9.grid(row=2, column=2)
 
-plus = Button(gui, text=' + ', fg='black', bg='red', command=lambda: press(" + "), height=2, width=2)
+plus = Button(gui, text=' + ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(" + "), height=2, width=2)
 plus.grid(row=1, column=3)
 
-minus = Button(gui, text=' - ', fg='black', bg='red', command=lambda: press(" - "), height=2, width=2)
+minus = Button(gui, text=' - ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(" - "), height=2, width=2)
 minus.grid(row=2, column=3)
 
-multiply = Button(gui, text=' * ', fg='black', bg='red', command=lambda: press(" * "), height=2, width=2)
+multiply = Button(gui, text=' ×️ ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(" * "), height=2, width=2)
 multiply.grid(row=3, column=3)
 
-divide = Button(gui, text=' / ', fg='black', bg='red', command=lambda: press(" / "), height=2, width=2)
+divide = Button(gui, text=' ÷ ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(" / "), height=2, width=2)
 divide.grid(row=4, column=3)
 
-divide = Button(gui, text=' ( ', fg='black', bg='red', command=lambda: press("( "), height=2, width=2)
+divide = Button(gui, text=' ( ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press("( "), height=2, width=2)
 divide.grid(row=1, column=1)
 
-divide = Button(gui, text=' ) ', fg='black', bg='red', command=lambda: press(" )"), height=2, width=2)
+divide = Button(gui, text=' ) ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=lambda: press(" )"), height=2, width=2)
 divide.grid(row=1, column=2)
 
-equal = Button(gui, text=' = ', fg='black', bg='red', command=equal_press, height=2, width=2)
+equal = Button(gui, text=' = ', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=equal_press, height=2, width=2)
 equal.grid(row=5, column=2)
 
-clear = Button(gui, text='Clear', fg='black', bg='red', command=clear, height=2, width=2)
+clear = Button(gui, text='CE', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', command=clear, height=2, width=2)
 clear.grid(row=1, column=0)
 
-blank1 = Button(gui, text='', fg='black', bg='red', height=2, width=2)
+blank1 = Button(gui, text='', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', height=2, width=2)
 blank1.grid(row=5, column=0)
 
-blank2 = Button(gui, text='', fg='black', bg='red', height=2, width=2)
+blank2 = Button(gui, text='', fg='white', highlightbackground='#E6E6E6', bg='#5F308C', height=2, width=2)
 blank2.grid(row=5, column=3)
 
 # start the GUI
